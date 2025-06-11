80df53835a0fc3546b4554b03d53deabccefc0e2
YasirDevOpsEngineer <153387850+yasirdevopseng@users.noreply.github.com>
2025-06-11 15:44:25 -0700

    Update installation-setup.sh

--- installation-setup.sh
+++ installation-setup.sh
@@ -30,3 +30,10 @@ gpg --no-default-keyring \
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt-get install terraform -y
+
+# ansible
+sudo apt update
+sudo apt install software-properties-common
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible -y
